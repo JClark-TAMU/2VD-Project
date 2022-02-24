4a63e149644f28e23850c4299b9b2acf2b40f3e8
arturovilla <acvturi@gmail.com>
2022-02-23 22:17:28 -0600

    deployment fix, had to do with scss gem and css compressor

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Webapp2vd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.css_compressor = nil
   end
 end
