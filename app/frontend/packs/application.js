72660e8aa16b5dd4888017cd3ef6954316d16b5c
root <root@localhost>
2022-03-06 22:56:17 +0000

    Will's changes that don't work yet but need css modification

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../js/bootstrap_js_files.js'  
+import '../js/bootstrap_js_files.js'
+import '../../assets/stylesheets/bootstrap.css'
+import 'bootstrap.js'
 
 Rails.start()
 Turbolinks.start()
