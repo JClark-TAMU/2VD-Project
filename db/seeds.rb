b3335fd61e66c1e6528a30181409e5edac2bc097
JClark-TAMU <jonathan.clark@tamu.edu>
2022-03-05 23:38:31 -0600

    Added conditional to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-User.create!([
-  {username: "OASIS", email: "jonathan.clark@tamu.edu", isAdmin: true, role: "Developer", bio: "I code", portfolioID: nil, full_name: "Jonathan Clark", avatar: "https://lh3.googleusercontent.com/a-/AOh14GjKHilpdz9o-rWwR26k4y2VHT8w-Ih3JViRGv4pAw=s96-c"}
-])
+if !User.exists?(email: "jonathan.clark@tamu.edu")
+  User.create!([
+    {username: "OASIS", email: "jonathan.clark@tamu.edu", isAdmin: true, role: "Developer", bio: "I code", portfolioID: 1, full_name: "Jonathan Clark", avatar: "https://lh3.googleusercontent.com/a-/AOh14GjKHilpdz9o-rWwR26k4y2VHT8w-Ih3JViRGv4pAw=s96-c"}
+  ])
+  Portfolio.create!(title: "OASIS", user_id: 1)
+end
