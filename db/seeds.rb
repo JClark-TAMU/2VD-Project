d20818e4be5053e3be4f98a7f89b20028132def8
JClark-TAMU <jonathan.clark@tamu.edu>
2022-03-05 22:45:21 -0600

    Cut down to only the user record

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-Portfolio.create!([
-  {title: "untitled", user_id: 1}
-])
 User.create!([
   {username: "OASIS", email: "jonathan.clark@tamu.edu", isAdmin: true, role: "Developer", bio: "I code", portfolioID: nil, full_name: "Jonathan Clark", avatar: "https://lh3.googleusercontent.com/a-/AOh14GjKHilpdz9o-rWwR26k4y2VHT8w-Ih3JViRGv4pAw=s96-c"}
 ])
